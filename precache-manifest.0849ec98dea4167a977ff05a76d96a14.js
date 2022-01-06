df9476d60537dcaa5d8def58bbd1b746ae2f8aae
Marcio <13090422+marciob@users.noreply.github.com>
2022-01-06 09:39:33 -0300

    Updates

---
+++ precache-manifest.0849ec98dea4167a977ff05a76d96a14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "876ef327cc43ea67f8f3",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.876ef327.chunk.js"
+  },
+  {
+    "revision": "c13762baa940c1dabfb3",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/1.c13762ba.chunk.js"
+  },
+  {
+    "revision": "2242dfc52fd5369ed931",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime~main.2242dfc5.js"
+  },
+  {
+    "revision": "0220f9b0f226eb2df13c24f6eb7a4086",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  }
+];
